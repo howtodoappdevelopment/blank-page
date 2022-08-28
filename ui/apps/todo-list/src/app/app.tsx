287c2d2695058fd15f9932e95578b5194e655a66
daniel <howtodoappdevelopment@gmail.com>
2022-08-28 10:33:30 +0200

    chore: storybook and tailwind css setup with button initial config

--- ui/apps/todo-list/src/app/app.tsx
+++ ui/apps/todo-list/src/app/app.tsx
@@ -1,10 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import { Button } from './components/button';
 
 export function App() {
   return (
     <>
-      <div />
+      <span>{"I'm working"}</span>
+      <Button>My button</Button>
     </>
   );
 }
--- ui/apps/todo-list/tailwind.config.js
+++ ui/apps/todo-list/tailwind.config.js
@@ -11,7 +11,16 @@ module.exports = {
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        main: 'var(--main)',
+        background: 'var(--background)',
+        header: 'var(--header)',
+        accent: 'var(--accent)',
+      },
+    },
   },
   plugins: [],
 };
