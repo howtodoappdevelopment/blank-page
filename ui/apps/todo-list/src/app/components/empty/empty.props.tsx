a31afdf42639173f907378ec0778d2bfd652a2d9
daniel <howtodoappdevelopment@gmail.com>
2022-08-31 08:40:25 +0200

    feat(todo-list): add empty component

--- ui/apps/todo-list/src/app/components/empty/empty.props.tsx
+++ ui/apps/todo-list/src/app/components/empty/empty.props.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 
 export type EmptyProps = {
-  icon: ReactNode | string;
-  description: ReactNode | string;
+  image?: ReactNode | string;
+  description?: ReactNode | string;
 };
