9d9db7987c6bc04271449f5baa94a79fbf6b5ded
daniel <howtodoappdevelopment@gmail.com>
2022-09-09 08:43:24 +0200

    feat: stable quote

--- ui/apps/todo-list/src/app/components/markdown-input/elements/quote/quote.parser.tsx
+++ ui/apps/todo-list/src/app/components/markdown-input/elements/quote/quote.parser.tsx
@@ -1,7 +1,18 @@
 import { Quote } from './quote';
 import { ParserType } from '../../type';
+import { removeSign } from './utils';
+import { countTabs } from '../utils';
+import { config } from '../../config';
+import { uniqueId } from 'lodash-es';
 
 export const QUOTE_PARSER: ParserType = {
-  regex: /^((( )?)+> .*(\n|$))+/gm,
-  parse: (txt: string) => <Quote>{txt}</Quote>,
+  regex: /^( ?)+> .*(\n|$)/gm,
+  parse: (txt: string) => (
+    <Quote
+      key={uniqueId('quote')}
+      indentPx={countTabs(txt) * config.tabIndentPx}
+    >
+      {removeSign(txt)}
+    </Quote>
+  ),
 };
--- ui/apps/todo-list/src/app/components/markdown-input/elements/quote/quote.tsx
+++ ui/apps/todo-list/src/app/components/markdown-input/elements/quote/quote.tsx
@@ -1,11 +1,16 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, PropsWithChildren } from 'react';
 
-export const Quote: FunctionComponent<{ children: string }> = ({
-  children,
-}) => {
+export const Quote: FunctionComponent<
+  PropsWithChildren<{ indentPx?: number }>
+> = ({ children, indentPx = 0 }) => {
   return (
-    <p className="border-l-4 border-gray-200 pl-2 bg-gray-100">
-      {children.replace('> ', '')}
+    <p
+      className="border-l-4 border-gray-200 pl-2 bg-gray-100"
+      style={{
+        marginLeft: `${indentPx}px`,
+      }}
+    >
+      {children}
     </p>
   );
 };
