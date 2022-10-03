cb0e51090c3bf8be22e4c6ab024680456ed7d9c2
daniel <howtodoappdevelopment@gmail.com>
2022-10-03 14:59:58 +0200

    chore: add markdown mock

--- ui/apps/todo-list/src/app/app-main.component.tsx
+++ ui/apps/todo-list/src/app/app-main.component.tsx
@@ -10,7 +10,6 @@ import { Button } from './layouts/button/button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus';
 import { Modal } from './layouts/modal/modal';
-import { fullMarkdown } from '@pages/mocks/full-markdown.mock';
 import { createMarkdownInput } from '@blank-page/realtime-markdown';
 
 export const AppMainComponent: FunctionComponent<
@@ -28,8 +27,62 @@ export const AppMainComponent: FunctionComponent<
 
   const ref = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
-    const elm = document.getElementById('some-static-html');
     if (ref && ref.current) {
+      const fullMarkdown = `
+random text
+# Heading 1
+## Heading 2
+### Heading 3
+#### Heading 4
+##### Heading 5
+###### Heading 6
+random text
+ \`random text\`
+  - random text
+  ### random text
+- [ ] non filled
+ - [ ] non filled (0 lvl)
+   - [x] child
+     - [ ] child ||
+     - [ ] child II.5
+       - [x] child |||
+         - [ ] child IV
+          - [ ] child V
+- [x] filled
+ - [ ] filled child
+> my **block quote**
+    > test
+> separate
+\`\`\`
+\`\`\`
+\`\`\`
+\`\`\`
+\`\`\`
+code block
+\`\`\`
+- unordered 1
+ - un 2
+  - un 2.1
+   - un 3
+* unor *dered 2*
++ unordered
+1. ordered
+  - nested
+2. ordered
+  > nested
+# Heading 1 x 2
+http://url.com
+[url](http://url.com)
+[url-id][]
+[example][url-id-2]
+[url-id]: http://test.com
+[url-id]: http://test2.com
+![Alt](https://picsum.photos/200/300)
+![Alt](https://picsum.photos/200/300 "Title")
+  ~~strike trough~~
+ ==highlight==
+<span style="border: 1px solid red">my custom span</span>`;
+
       Array.from(
         ref.current?.getElementsByClassName('content') || []
       )[0]?.replaceChildren(createMarkdownInput(fullMarkdown));
